af0bed309f67d131b0b5fc2962fd29426db9bd0d
Magesa jounior <137028972+Magesajr@users.noreply.github.com>
2024-09-29 14:26:33 +0300

    Update config.py

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -2,7 +2,7 @@ import os
 class Config:
   SECRET_KEY=os.environ.get('SECRET_KEY')
   PAYMENT_KEY=os.environ.get('PAYMENT_KEY')
-  SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
   MAIL_SERVER='smtp.gmail.com'
   MAIL_PORT=587
   MAIL_USE_TLS=True
