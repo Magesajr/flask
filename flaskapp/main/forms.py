2290abd724e74ff597482d1b082755742fb4961e
Magesa jounior <137028972+Magesajr@users.noreply.github.com>
2024-10-12 09:57:18 +0300

    Update forms.py

--- flaskapp/main/forms.py
+++ flaskapp/main/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from flaskapp.models import User
 from flask_wtf.file import FileField,FileAllowed
 from flask_login import current_user
-from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField,SelectField
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
 import os
 
