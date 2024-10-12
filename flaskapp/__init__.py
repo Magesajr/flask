9d7b409eef4262f30dc3f14972755d63066825ad
Magesa jounior <137028972+Magesajr@users.noreply.github.com>
2024-10-12 09:41:00 +0300

    Update __init__.py

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -3,9 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
 from flaskapp.config import  Config
+from flask_bootstrap import Bootstrap5
 
 
 db = SQLAlchemy()
+bootstrap=Bootstrap5()
 login_manager=LoginManager()
 login_manager.login_view='users.login'
 login_manager.login_message_category='info'
@@ -17,6 +19,7 @@ def create_app(config_class=Config):
   db.init_app(app)
   login_manager.init_app(app)
   mail.init_app(app)
+  bootstrap.init_app(app)
   
   from flaskapp.users.route import users
   from flaskapp.posts.route import posts
