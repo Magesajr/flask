8c1bee398a4ecc7e02b91f6d9784edecc19f9920
Magesa jounior <137028972+Magesajr@users.noreply.github.com>
2024-10-12 09:30:04 +0300

    Update forms.py

--- flaskapp/main/forms.py
+++ flaskapp/main/forms.py
@@ -1,1 +1,13 @@
+from flask_wtf import FlaskForm
+from flaskapp.models import User
+from flask_wtf.file import FileField,FileAllowed
+from flask_login import current_user
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
+from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
+import os
 
+folder='flaskapp/main'
+choices=[(f,f) for f in os.listdir(folder)]
+class DownloadForm(FlaskForm):
+  file=SelectField('check your File to download',choices=choices,validators-=[DataRequired()])
+  submit=SubmitField('download')
